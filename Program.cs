ccd9a65e8c3ee75006fa9783b495ea9dff4fa49a
Trần Trọng Nghĩa <nghiatrantrong188@gmail.com>
2023-04-01 20:55:09 +0700

    Giai Phuong Trinh Bac Nhat

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace GiaiPhuongTrinhBacNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giai Phuong Trinh Bac Nhat");
+            Console.WriteLine("Cho mot bien a = a * x + b = 0', xin hay nhap:");
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.Write("The solution is: {0}!", solution);
+            }
+            else
+            {
+                if (b == 0)
+                    {
+                        Console.Write("Phuong trinh co nghiem la: x!");
+                    }
+                else
+                    {
+                        Console.Write("Phuong trinh vo nghiem!");
+                    }
+            }
+        }
+    }
+}
